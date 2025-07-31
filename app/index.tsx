01f90b53c1b81f881a82f27cf27c176c22238f9d
0Hooni <thddudgns972@gmail.com>
2025-07-31 21:58:54 +0900

    chore: 초기 프로젝트 생성

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { View, Text } from "react-native";
+
+export default function IndexScreen() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
