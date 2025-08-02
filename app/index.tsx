b883a67b76700c8f725a09ba74bc0e7788059729
0Hooni <thddudgns972@gmail.com>
2025-08-02 17:11:50 +0900

    feat: 시작 화면에 애플, 구글 로그인 버튼 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
 import { StyleSheet } from "react-native";
 
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { AppleLoginButton, GoogleLoginButton } from "@components";
 
 export default function IndexScreen() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <AppleLoginButton />
+      <GoogleLoginButton />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 16,
+  },
+});
